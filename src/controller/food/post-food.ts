afc259329427db83aeaadc8c49e5e55a701ea5cd
25LP1749 25LP1749 <merkzadok@gmail.com>
2025-06-27 12:16:41 +0800

    a

--- src/controller/food/post-food.ts
+++ src/controller/food/post-food.ts
@@ -2,11 +2,9 @@ import { Request, Response } from "express";
 import Food from "../../model/food";
 
 export const createFood = async (req: Request, res: Response) => {
-  const { categoryName, foodName, price, ingredients, category, image } =
-    req.body;
+  const { foodName, price, ingredients, category, image } = req.body;
   try {
     const food = await new Food({
-      categoryName: categoryName,
       foodName: foodName,
       price: price,
       category: category,
